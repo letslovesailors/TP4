3c500192b0405a5d9f8bd574bf4f6c7b03d30a50
s4y0x <126882041+s4y0x@users.noreply.github.com>
2025-04-11 15:02:10 +0200

    login update

--- project-root/app/Views/login.php
+++ project-root/app/Views/login.php
@@ -6,6 +6,8 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Login</h1>
+    <form method="POST" action ="Login"><button>se connecter</button></form>
+        <input type="text", name="Nom d'utilisateur" placeholder="Nom d'utilisateur">
 </body>
 </html>
