48924ec90d22dcd2b3f2ae80d2d1adca21d2f1fe
s4y0x <126882041+s4y0x@users.noreply.github.com>
2025-04-11 15:17:07 +0200

    test

--- project-root/app/Views/login.php
+++ project-root/app/Views/login.php
@@ -7,7 +7,22 @@
 </head>
 <body>
     <h1>Login</h1>
-    <form method="POST" action ="Login"><button>se connecter</button></form>
-        <input type="text", name="Nom d'utilisateur" placeholder="Nom d'utilisateur">
+    <form method="POST" action ="Login"></form>
+        <label for="matricule">Login :</label>
+        <input type="text", name="Matricule abonné" placeholder="Matricule" required><br>
+        <label for="nom abonné">Abonné :</label>
+        <input type="text", name = "Nom abonné" placeholder="Nom abonnée" required> <br>
+        <button>Se connecter</button>
+    <style>
+        form {
+            text-align: center;
+            display: flex;
+        }
+        label {
+            text-align: center;
+            align-items: center;
+            display: flex;
+        }
+    </style>
 </body>
 </html>
